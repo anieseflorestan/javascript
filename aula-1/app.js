77ed718cbcb669832fe9571121f696610623b676
ANIESE FLORESTAN <aniese_florestan@estudante.sesisenai.org.br>
2026-07-03 17:16:03 -0300

    javascript

---
+++ aula-1/app.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+
+let numeroUm = 10;
+let numeroDois = 20;
+let nome = "Adalberto junior"
+let booleano = false;
+
+function somar(a, b){
+    return a + b;
+}
+
+console.log(numeroUm + numeroDois);
+
+console.log("Resultado:" + (numeroDois + numeroDois))
+
+console.log(` Resultado Interpolado: ${numeroUm + numeroDois} + "<--- Resultado"`)
+
+console.log(`Resultado: ${numeroUm + numeroDois} <--- Resultado`)
+console.log(`Soma: ${somar(6,7)} <---`);
+console.log(nome)
